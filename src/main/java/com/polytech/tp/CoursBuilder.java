45a52d61895bd6c5fee9cb596683f75006ac7ac0
Cherarra <votreemail@votreemail.com>
2025-11-20 13:56:05 +0100

    exercice 1

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -2,13 +2,25 @@ package com.polytech.tp;
 
 public class CoursBuilder {
     // TODO: Implémenter le pattern Builder
+    String matiere;
+    String enseignant;
+    String salle;
+    String date;
+    String heureDebut;
+    boolean estOptionnel;
+    String niveau;
+    boolean necessiteProjecteur;
     
-    
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
+    public CoursBuilder setMatiere(String matiere) { 
+        this.matiere = matiere;
+        return this;}
+    public CoursBuilder setEnseignant(String enseignant) {
+        this.enseignant = enseignant;
+        return this;
+    }
     
     
     public Cours build() {
-        return null;
+  return new Cours(this);
     }
 }
